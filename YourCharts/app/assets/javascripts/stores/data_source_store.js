3a1393179dbd88c22b0d9d5926945d6eac10cf3d
Anthony Ira Ladson <aladsonjr@gmail.com>
2015-10-12 22:33:37 -0700

    implmetrics extracted from data source store

--- YourCharts/app/assets/javascripts/stores/data_source_store.js
+++ YourCharts/app/assets/javascripts/stores/data_source_store.js
@@ -25,6 +25,33 @@
       return _dataSources.map(function(source){ return source.name; });
     },
 
+    metrics: function(){
+      var source = _selectedDataSource.data[0];
+      var m = [];
+
+      for(var metric in source){
+        if(source.hasOwnProperty(metric)){
+          m.push({
+            metric: metric,
+            type: this.guessType(source[metric])
+          });
+        }
+      }
+
+      return m;
+    },
+
+    guessType: function(val){
+      var floated = parseFloat(val);
+
+      //NaN !== NaN || coersion to float would strip letters from val
+      if(floated !== floated || (floated + "").length !== (val+"").length){
+        return "string";
+      } else {
+        return "number";
+      }
+    },
+
     addChangeHandler: function(cb){
       this.on(CHANGE, cb);
     },
